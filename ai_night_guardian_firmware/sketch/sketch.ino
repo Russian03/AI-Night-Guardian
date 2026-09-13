7c0363a42f0141d58f245ab37f89560a4c6c342f
Russian03 <jan.diazcc03@gmail.com>
2026-09-13 04:27:44 +0200

    app last version

--- ai_night_guardian_firmware/sketch/sketch.ino
+++ ai_night_guardian_firmware/sketch/sketch.ino
@@ -1,7 +1,5 @@
 #include <Arduino_Modulino.h>
 #include <Arduino_RouterBridge.h>
-#include <zephyr/zephyr.h>
-#include <zephyr/drivers/i2s.h>
 
 ModulinoThermo thermo;
 ModulinoLight light;
@@ -25,34 +23,16 @@ void setup() {
   Serial.begin(115200);
 
   Modulino.begin();
-
   thermo.begin();
   light.begin();
 
   Bridge.begin();
   Bridge.provide("read_sensors", read_sensors);
 
-  if (!I2S.begin(I2S_PHILIPS_MODE, 16000, 32)) {
-    Serial.println("Error al iniciar el microfono");
-    while (1);
-  }
-
   Serial.println("Sensores y bridge iniciados");
 }
 
 void loop() {
   light.update();
-
-  Serial.print("Temperatura: ");
-  Serial.print(thermo.getTemperature());
-  Serial.print(" C | Humedad: ");
-  Serial.print(thermo.getHumidity());
-  Serial.print(" % | Lux: ");
-  Serial.println(light.getLux());
-  int32_t muestra_audio = I2S.read();
-  if (muestra_audio) {
-    Serial.println(muestra_audio);
-  }
-
   delay(2000);
 }
